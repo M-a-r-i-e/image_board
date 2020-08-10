8b1b138c1ff3c7915aded75fc66bfb396fb49d8e
System Administrator <root@Maries-MacBook-Pro-2.local>
2020-08-10 10:42:11 +0200

    s3

--- s3.js
+++ s3.js
@@ -1,6 +1,10 @@
 
-
-const secrets = require("./secrets.json")
+let secrets;
+    if (process.env.NODE_ENV == 'production') {
+        secrets = process.env; // in prod the secrets are environment variables
+    } else {
+        secrets = require('./secrets.json'); // in dev they are in secrets.json which is listed in .gitignore
+    }                
 
 const aws = require("aws-sdk");
 
